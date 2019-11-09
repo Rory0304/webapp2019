6da90b97b98fdb137bb963e349445eb07b452538
Sa Eun Soo <tkdmstn0304@naver.com>
2019-11-10 01:02:08 +0900

    changeFont

--- selab/courses/cse326/2019/lecture/lec.js
+++ selab/courses/cse326/2019/lecture/lec.js
@@ -6,33 +6,33 @@ var snum = 0;
 
 // function for function get ready
 function nodeValue(node) {
-    var result = "";
-    if (node.nodeType == 1) {
-        var children = node.childNodes;
-        for (var i = 0; i < children.length; ++i) {
-            result += nodeValue(children[i]);
-        }
+  var result = "";
+  if (node.nodeType == 1) {
+    var children = node.childNodes;
+    for (var i = 0; i < children.length; ++i) {
+      result += nodeValue(children[i]);
     }
-    else if (node.nodeType == 3) {
-        result = node.nodeValue;
-    }
-    return(result);
+  }
+  else if (node.nodeType == 3) {
+    result = node.nodeValue;
+  }
+  return(result);
 }
 
 function hasClass(object, className) {
-    if (!object.className) return false;
-    return (object.className.search('(^|\\s)' + className + '(\\s|$)') != -1);
+  if (!object.className) return false;
+  return (object.className.search('(^|\\s)' + className + '(\\s|$)') != -1);
 }
 
 function GetElementsWithClassName(elementName,className) {
-    var allElements = document.getElementsByTagName(elementName);
-    var elemColl = new Array();
-    for (var i = 0; i< allElements.length; i++) {
-        if (hasClass(allElements[i], className)) {
-            elemColl[elemColl.length] = allElements[i];
-        }
+  var allElements = document.getElementsByTagName(elementName);
+  var elemColl = new Array();
+  for (var i = 0; i< allElements.length; i++) {
+    if (hasClass(allElements[i], className)) {
+      elemColl[elemColl.length] = allElements[i];
     }
-    return elemColl;
+  }
+  return elemColl;
 }
 
 // function that do the actual work
@@ -42,16 +42,16 @@ function editHtml() {
   var layoutDiv = document.getElementsByClassName("layout");
   var layoutArr = (layoutDiv[0].innerHTML).split(/\n/g);
   layoutDiv[0].innerHTML = '<div id="side">' +
-   layoutArr[1] +
-   '<div id="navList"><ul id="index" ><\/ul><\/div>' + '<div id="custom"><div id="background"></div> '+
-  '<div id="font"><select id="fontselect" name="selectFontFamily" onchange="fontupdate();"><option> Serif </option><option> Tahoma </option><option> Arial </option></select></div></div>' +
-   layoutArr[4] + layoutArr[5] + layoutArr[6] + layoutArr[7]
-   +'</div><div id="line"> </div>' +  '<a href="javascript:sideFold()"> <div id="fold"><p>||</p></div> </a>';
+  layoutArr[1] +
+  '<div id="navList"><ul id="index" ><\/ul><\/div>' + '<div id="custom"><div id="background"></div> '+
+  '<div id="font"></div></div>' +
+  layoutArr[4] + layoutArr[5] + layoutArr[6] + layoutArr[7]
+  +'</div><div id="line"> </div>' +  '<a href="javascript:sideFold()"> <div id="fold"><p>||</p></div> </a>';
 }
 
 function createControls() {
-    var controlsDiv = document.getElementById("controls");
-    if (!controlsDiv) return;
+  var controlsDiv = document.getElementById("controls");
+  if (!controlsDiv) return;
 
     controlsDiv.innerHTML = //'<form action="#" id="controlForm">' +
     '<div id="navaLinks">' +
@@ -62,36 +62,36 @@ function createControls() {
     '<a href="javascript:customize();" /> <img src = "images/font.png"/> </a>' +
     '<a href="javascript:oneByOne(0);" /> <img src = "images/vertical.png"/> </a></div>';
     //+ '<\/form>';
-}
+  }
 
-function slideLabel() {
+  function slideLabel() {
     var slideColl = GetElementsWithClassName('*', 'slide');
     var index = document.getElementById('index');
     smax = slideColl.length;
 
     for (var n = 0; n < smax; n++) {
-        var obj = slideColl[n];
-        obj.setAttribute('id', 'slide' + n);
+      var obj = slideColl[n];
+      obj.setAttribute('id', 'slide' + n);
         // if (isOp) continue;
 
         var otext = '';
         var menu = obj.firstChild;
         if (!menu) continue; // to cope with empty slides
         while (menu != null && menu.nodeType == 3) {
-            menu = menu.nextSibling;
+          menu = menu.nextSibling;
         }
         if (!menu) continue; // to cope with slides with only text nodes
 
         var menunodes = menu.childNodes;
         for (var o = 0; o < menunodes.length; o++) {
-            otext += nodeValue(menunodes[o]);
+          otext += nodeValue(menunodes[o]);
         }
 
         otext = otext.replace(/\r/g, "");
         otext = otext.replace(/\n/g, "");
         var value = n + ':' + otext ;
         if (hasClass(obj, 'titleslide') || hasClass(obj, 'lecturetitle')) {
-            value = n + " : === " + otext + " ===";
+          value = n + " : === " + otext + " ===";
         }
 
         var li = document.createElement("li");
@@ -104,26 +104,26 @@ function slideLabel() {
 
         index.appendChild(link);
       }
-}
+    }
 
-function currentSlide() {
-    var sn = document.getElementById('slideNum');
+    function currentSlide() {
+      var sn = document.getElementById('slideNum');
 
-    sn.innerHTML = '<span id="snHere">' + snum + '<\/span> ' +
-        '<span id="snSep">\/<\/span> ' +
-        '<span id="snTotal">' + (smax-1) + '<\/span>';
-}
+      sn.innerHTML = '<span id="snHere">' + snum + '<\/span> ' +
+      '<span id="snSep">\/<\/span> ' +
+      '<span id="snTotal">' + (smax-1) + '<\/span>';
+    }
 
 //function for specific func
 
 //function for fold something
 function fold(id){
-    var elem = document.getElementById(id);
-    if(elem.style.display=='none'){
-      elem.style.display = 'block';
-    }else{
-      elem.style.display = 'none';
-    }
+  var elem = document.getElementById(id);
+  if(elem.style.display=='none'){
+    elem.style.display = 'block';
+  }else{
+    elem.style.display = 'none';
+  }
 }
 
 function pptResizing() {
@@ -218,54 +218,55 @@ function oneByOne(n) {
 }
 
 function standardizeEvent(event) {
-    var e = event || window.event;
+  var e = event || window.event;
     // e.which = e.button || e.which;
     if (!e.keyCode) {
-        try {
-            e.keyCode = e.which;
-        } catch (ex) {}
+      try {
+        e.keyCode = e.which;
+      } catch (ex) {}
     }
     if (!e.srcElement) {
-        try {
-            e.srcElement = e.target;
-        } catch (ex) {}
+      try {
+        e.srcElement = e.target;
+      } catch (ex) {}
     }
     return e;
-}
+  }
 
 // 'keys' code adapted from MozPoint (http://mozpoint.mozdev.org/)
 function keys(event) {
   if (OBO == false)
     return;
 
-    event = standardizeEvent(event);
-    var modifierKey = event.ctrlKey || event.altKey || event.metaKey;
+  event = standardizeEvent(event);
+  var modifierKey = event.ctrlKey || event.altKey || event.metaKey;
 
-    switch (event.keyCode) {
+  switch (event.keyCode) {
       case 32: // spacebar
       case 34: // page down
       case 39: // rightkey
       case 40: // downkey
-          if (!modifierKey) {
-            oneByOne(1);
-          }
-          break;
+      if (!modifierKey) {
+        oneByOne(1);
+      }
+      break;
       case 33: // page up
       case 37: // leftkey
       case 38: // upkey
-          if (!modifierKey) {
-            oneByOne(-1);
-          }
-          break;
+      if (!modifierKey) {
+        oneByOne(-1);
+      }
+      break;
       case 36: // home
-          oneByOne(-snum);
-          break;
+      oneByOne(-snum);
+      break;
       case 35: // end
-          oneByOne(smax-1-snum);
-          break;
+      oneByOne(smax-1-snum);
+      break;
     }
-}
+  }
 
+//function for showing customized page
 function customize(){
   var custom = document.getElementById("custom");
   var layout = document.getElementsByClassName("layout");
@@ -274,11 +275,43 @@ function customize(){
     custom.style.display = 'block';
     layout[0].style.width = "50%";
     navList.style.width = "50%";
+    font();
   }
   else{
    custom.style.display = '';
    layout[0].style.width = "25%";
    navList.style.width = '';
+ }
+}
+
+function font(){
+
+  var fontdiv = document.getElementById("font");
+  fontdiv.innerHTML = '<p>Font</p>' + 
+  '<select id="fontselect" name="selectFontFamily" onchange="fontupdate(1);">' + 
+  '<option> Serif </option><option> Tahoma </option><option> Arial </option><option>Monospace</option>' + 
+  '<option>Roboto</option><option>Times New Roman</option><option>Garamond</option>' +
+  '<option>Comic Sans MS</option><option>Courier</option><option>Impact</otpion></select>' + 
+  '<p>Font Size</p>' + 
+  '<select id="fontsizeSlect" name="selectfontsize" onchange="fontupdate(2);">' + 
+  '<option>10px</option><option>12px</option><option>14px</option><option>16px</option><option>18px</option>' + 
+  '<option>20px</option><option>22px</option><option>24px</option><option>26px</option><option>28px</option>' + 
+  '<option>30px</option></select>'
+  ; 
+}
+
+function fontupdate(value){
+  if(value==1){
+   var font = document.getElementById("fontselect").value;
+   document.getElementsByClassName('presentation')[0].style.fontFamily = font;
+ }
+ if(value==2){
+   var fontsize = document.getElementById("fontsizeSlect").value;
+   var slides =  document.getElementsByClassName("slide");
+   var i;
+   for (i = 0; i < slides.length; i++) {
+    slides[i].style.fontSize = fontsize;
+   }
   }
 }
 
