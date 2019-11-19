22f9b6b7a8c84a4af172938d748b1262806b0fbe
Unknown <kth2642@naver.com>
2019-11-19 10:56:36 +0900

    login 보완

--- selab/login/login.php
+++ selab/login/login.php
@@ -19,8 +19,11 @@
 				if ($result[id] == $id)
 					if ($result[password] == $pw)
 						Header("Location:index.html"); //프로젝트에는 실제 index.html 경로를 입력
+					else
+						Header("Location:index4e7d.html"); // 위와 마찬가지
 			}
-			Header("Location:index4e7d.html"); // 위와 마찬가지
+			else
+				Header("Location:index4e7d.html"); // 위와 마찬가지
     }
     catch (PDOException $ex) {
     ?>
