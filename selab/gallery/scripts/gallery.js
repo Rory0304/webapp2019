6aa635f6e77ec9c49da42251897bdf37e1b6ebb0
Jiseon-Lee <l990831@naver.com>
2019-11-23 15:18:19 +0900

    change gallery

--- selab/gallery/scripts/gallery.js
+++ selab/gallery/scripts/gallery.js
@@ -3,12 +3,11 @@ $(document).ready(function () {
 	$("#cancel").click(clickCancelBtn);
 	$("#submit").click(clickSubmitBtn);
 	$(".delete").click(clickDeleteBtn);
+	filterSelection("all");
 	$("#tab").find('div').click(function(){
 		$('#tab').find('div').addClass("deactive");
 		$(this).removeClass("deactive");
-		$('.back').addClass('hidden');
-		$var = $(this).data('type');
-		$('#'+$var).removeClass('hidden');
+		filterSelection(this.dataset.type);
 	});
 });
 
@@ -28,7 +27,7 @@ function clickSubmitBtn () {
 	} else{
 		$.ajax();
 	}
-	
+
 }
 
 function clickDeleteBtn(){
@@ -39,4 +38,35 @@ function clickDeleteBtn(){
 	}).done(function(msg){
 		location.reload();
 	});
+}
+
+function filterSelection(c) {
+  var x, i;
+  x = document.getElementsByClassName("column");
+  if (c == "all") c = "";
+  for (i = 0; i < x.length; i++) {
+    w3RemoveClass(x[i], "show");
+    if (x[i].className.indexOf(c) > -1) w3AddClass(x[i], "show");
+  }
+}
+
+function w3AddClass(element, name) {
+  var i, arr1, arr2;
+  arr1 = element.className.split(" ");
+  arr2 = name.split(" ");
+  for (i = 0; i < arr2.length; i++) {
+    if (arr1.indexOf(arr2[i]) == -1) {element.className += " " + arr2[i];}
+  }
+}
+
+function w3RemoveClass(element, name) {
+  var i, arr1, arr2;
+  arr1 = element.className.split(" ");
+  arr2 = name.split(" ");
+  for (i = 0; i < arr2.length; i++) {
+    while (arr1.indexOf(arr2[i]) > -1) {
+      arr1.splice(arr1.indexOf(arr2[i]), 1);
+    }
+  }
+  element.className = arr1.join(" ");
 }
