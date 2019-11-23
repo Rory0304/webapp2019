10e98e3585517f0238f47da1d38fc0b4d3455bcc
Sa Eun Soo <tkdmstn0304@naver.com>
2019-11-24 03:03:04 +0900

    error fix and customize font size

--- selab/common/scripts/common.js
+++ selab/common/scripts/common.js
@@ -11,23 +11,124 @@ function closesetting() {
 }
 
 function fontupdate(value){
-  if(value==1){
-   var font = document.getElementById("fontselect").value;
-   document.getElementById("main").style.fontFamily = font;
-   document.getElementById("samplePage").style.fontFamily = font;
-  }
-  if(value==2){
-    var fontsize = document.getElementById("fontsizeSlect").value;
-    document.getElementById("sortable").style.fontSize = fontsize;
-    document.getElementById("sampleC").style.fontSize = fontsize;
-  }
+  jQuery(function($) {
+    if(value=='fontstyle'){
+      var font = $("#fontselect").val();
+      $("#main").css("fontFamily",font);
+      $("#samplePage").css("fontFamily",font);
+    }
+
+    else if(value=='home'){
+      var select = $("#fontsizeSlect").val();
+      var fontsize = titleSize(select);
+      $("#sortable").css("font-size",fontsize);
+      $("#sampleC").css("font-size",fontsize);
+    }
+
+    else if(value=='notice'){
+      var select = $("#fontsizeSlect").val();
+      var title = titleSize(select);
+      var content = contentSize(select);
+      $(".title pull-left").css("font-size",title);
+      $(".name pull-left").css("font-size",title);
+      $(".time pull-left").css("font-size",title);
+      $(".entry").css("font-size",content);
+    }
+
+    else if(value=='members'){
+      var select = $("#fontsizeSlect").val();
+      var title = titleSize(select);
+      var content = contentSize(select);
+      $(".position h2").css("font-size",title);
+      $(".name").css("font-size",title);
+      $("#main ul").css("font-size",content);
+    }
+
+     else if(value=='research'){
+      var select = $("#fontsizeSlect").val();
+      var title = titleSize(select);
+      var content = contentSize(select);
+      $("#tab div").css("font-size",title);
+      $(".contents h3").css("font-size",title);
+      $(".subject li").css("font-size",content);
+    }
+
+    else if(value=='publication'){
+      var select = $("#fontsizeSlect").val();
+      var title = titleSize(select);
+      var content = contentSize(select);
+      $("#tab div").css("font-size",title);
+      $("#publications td").css("font-size",content);
+    }
+
+    else if(value=='gallery'){
+     var select = $("#fontsizeSlect").val();
+     var title = titleSize(select);
+     var content = contentSize(select);
+     $("#tab div").css("font-size",title);
+     $(".title").css("font-size",content);
+   }
+ });
 }
 
- function backupdate(){
+function backupdate(){
   var back = document.getElementById("backselect").value;
   var backcss = document.getElementById("backcss");
   backcss.href=('home/styles/theme' + back +'.css');
- }
+}
+
+function titleSize(select){
+  switch(select){
+    case 'Small':
+    var result =  '15px';
+    break;
+
+    case 'Normal':
+    var result =  '20px';
+    break;
+
+    case 'Medium':
+    var result ='25px';
+    break;
+
+    case 'Large':
+    var result =  '30px';
+    break;
+
+    case 'Huge':
+    var result = '35px';
+    break;
+  }
+
+  return result;
+}
+
+function contentSize(select){
+  switch(select){
+    case 'Small':
+    var result =  '13px';
+    break;
+
+    case 'Normal':
+    var result =  '18px';
+    break;
+
+    case 'Medium':
+    var result ='23px';
+    break;
+
+    case 'Large':
+    var result =  '28px';
+    break;
+
+    case 'Huge':
+    var result = '32px';
+    break;
+  }
+
+  return result;
+}
+
 
 function navigation() {
   jQuery(function($) {
