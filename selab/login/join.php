adb433ce400bd6cbb50cc03f403e25064e1b20a9
Unknown <pyl9983@naver.com>
2019-12-10 13:05:45 +0900

    fix

--- selab/login/join.php
+++ selab/login/join.php
@@ -1,13 +1,14 @@
 <?php
-include "../../../common/db.php";
+include "../common/db.php";
 
 if($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['name']) && isset($_POST['num']) && isset($_POST['class'])){
   $name = $_POST['name'];
   $num = $_POST['num'];
   $class = $_POST['class'];
 
-  //$insertQ = $db-> query("insert into member (studentNum,class,name) values ('$num',$class,'$name')");
-  //$insert = $db->query("insert into lang (studentNum,teamNum,html,css,js,jquery,php,db,ajax,xml,c,cplusplus,java,python,ruby,Django,Perl,etc) values ('$num',null,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,null");
+  $insertQ = $db-> query("insert into member (studentNum,class,name) values ('$num',$class,'$name')");
+  $a = "insert into lang (studentNum,teamNum,html,css,js,jquery,php,db,ajax,xml,c,cplusplus,java,python,ruby,Django,Perl,etc) values ('$num',null,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,null)";
+  $insert = $db->query($a);
 
   echo "<script>location.replace('index4e7d.html')</script>";
 } ?>
