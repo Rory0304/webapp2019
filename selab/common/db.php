43a229b3babb6d521c6f8a4cc91f504f24f4774e
YunSungJu <bravokan7@gmail.com>
2019-12-13 13:33:04 +0900

    change db adress, da charset utf-8

--- selab/common/db.php
+++ selab/common/db.php
@@ -1,12 +1,12 @@
 <?php
 //$dbHost = "52.78.148.203";
-$dbHost = "localhost";
-$dbName = "test";
+$dbHost = "54.180.112.225";
+$dbName = "team";
 $dbUser = "root";
-$dbPass = "root";
-
+$dbPass = "1111";
+// $db = new PDO("mysql:dbname=custom;host=54.180.112.225;port=3306","root","1111"); //프로젝트에는 실제 mysql의 id, password 입력
 //$db = new PDO("mysql:host={$dbHost};dbname={$dbName}; port=3306", $dbUser, $dbPass);
-$db = new PDO("mysql:host={$dbHost};dbname={$dbName};", $dbUser, $dbPass);
+$db = new PDO("mysql:host={$dbHost};dbname={$dbName};port=3306", $dbUser, $dbPass);
 $db->exec("set names utf8");
 
 $langList = array("html","css","js","jquery","php","db","ajax","xml","c","c++","java","python","ruby","Django","Perl");
