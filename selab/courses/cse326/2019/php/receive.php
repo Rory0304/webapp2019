bc03cd3d31c76e349269a1ce6073cb27dbad54d7
Unknown <pyl9983@naver.com>
2019-12-20 06:49:15 +0900

    fix

--- selab/courses/cse326/2019/php/receive.php
+++ selab/courses/cse326/2019/php/receive.php
@@ -21,15 +21,8 @@
         var_dump($q_day);
         echo "</pre>";
 
-        #sender 의 teamname
-        $check = "SELECT * FROM member WHERE studentNum = $q_sender";
-        $rows = $db->query($check);
-        $results = $rows->fetchAll();
-        $teamname = $results[0]["teamname"];
-        $q_teamname = $db->quote($teamname);
-
         echo "<pre>";
-        var_dump($q_teamname);
+        var_dump($q_sender);
         echo "</pre>";
 
         #나의 teamname
@@ -48,17 +41,17 @@
             $results = $rows->fetchAll();
             if (count($results) == 1) {
                 $db->exec("DELETE FROM team
-                        WHERE 
+                        WHERE
                         teamname = $q_myteam");
             }
         }
 
         $db->exec("UPDATE member SET
-                    teamname=$q_teamname
+                    teamname=$q_sender
                     WHERE studentNum = $q_mynum");
 
         $db->exec("DELETE FROM message
-                    WHERE 
+                    WHERE
                     sender = $q_sender and
                     receiver = $q_mynum and
                     sendDay = $q_day");
